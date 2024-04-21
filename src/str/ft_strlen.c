5e342060b8cca72d420aa7c6470a867ad3b29e34
y-syo <mmoussou@student.42angouleme.fr>
2024-04-22 01:17:25 +0200

    [🔨] fix(ft_strlen): optimisation

--- src/str/ft_strlen.c
+++ src/str/ft_strlen.c
@@ -3,23 +3,19 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlen.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mmoussou <mmoussou@student.42angoulem      +#+  +:+       +#+        */
+/*   By: mmoussou <mmoussou@student.42angouleme.fr  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/31 12:42:36 by mmoussou          #+#    #+#             */
-/*   Updated: 2023/11/13 19:42:26 by mmoussou         ###   ########.fr       */
+/*   Updated: 2024/04/16 18:09:42 by mmoussou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
-
-size_t	ft_strlen(char const	*s)
+uint	ft_strlen(const char *s)
 {
-	int	i;
+	const char	*endptr;
 
-	i = 0;
-	if (!s)
-		return (0);
-	while (s[i])
-		i++;
-	return (i);
+	endptr = s;
+	while (*endptr)
+		endptr++;
+	return (endptr - s);
 }
